dc08886e73d5a46cfee5003d55cad51a0d475202
Terry FINKEL <nfinkel@student.42.fr>
2019-03-21 17:43:44 +0100

    Fix Grace and Sully in ASM (fopen return check)

--- ASM/Grace.s
+++ ASM/Grace.s
@@ -1,4 +1,4 @@
-%define S "%%define S %1$c%3$s%1$c,0%2$c%%define P %1$c%4$s%1$c,0%2$c%%macro M 0%2$csection .data%2$cr db %1$cw%1$c%2$cp db P%2$cs db S%2$csection .text%2$cextern _exit%2$cextern _fclose%2$cextern _fopen%2$cextern _fprintf%2$cglobal _main%2$c_main:%2$cpush rbp%2$cmov rbp,rsp%2$clea rdi,[rel p]%2$clea rsi,[rel r]%2$ccall _fopen%2$cmov rbx,rax%2$cmov rdi,rbx%2$clea rsi,[rel s]%2$cmov rdx,34%2$cmov rcx,10%2$cmov r8,rsi%2$clea r9,[rel p]%2$ccall _fprintf%2$cmov rdi,rbx%2$ccall _fclose%2$ccall _exit%2$c%%endmacro%2$c;Start of main%2$cM%2$c",0
+%define S "%%define S %1$c%3$s%1$c,0%2$c%%define P %1$c%4$s%1$c,0%2$c%%macro M 0%2$csection .data%2$cr db %1$cw%1$c%2$cp db P%2$cs db S%2$csection .text%2$cextern _exit%2$cextern _fclose%2$cextern _fopen%2$cextern _fprintf%2$cglobal _main%2$c_main:%2$cpush rbp%2$cmov rbp,rsp%2$clea rdi,[rel p]%2$clea rsi,[rel r]%2$ccall _fopen%2$ccmp rax,0%2$cjz .exit%2$cmov rbx,rax%2$cmov rdi,rbx%2$clea rsi,[rel s]%2$cmov rdx,34%2$cmov rcx,10%2$cmov r8,rsi%2$clea r9,[rel p]%2$ccall _fprintf%2$cmov rdi,rbx%2$ccall _fclose%2$c.exit:%2$ccall _exit%2$c%%endmacro%2$c;Start of main%2$cM%2$c",0
 %define P "Grace_kid.s",0
 %macro M 0
 section .data
@@ -17,6 +17,8 @@ mov rbp,rsp
 lea rdi,[rel p]
 lea rsi,[rel r]
 call _fopen
+cmp rax,0
+jz .exit
 mov rbx,rax
 mov rdi,rbx
 lea rsi,[rel s]
@@ -27,6 +29,7 @@ lea r9,[rel p]
 call _fprintf
 mov rdi,rbx
 call _fclose
+.exit:
 call _exit
 %endmacro
 ;Start of main
