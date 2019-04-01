8dee35331404046d0e260ab0bcd4b08c8a700a84
Terry FINKEL <nfinkel@student.42.fr>
2019-04-01 23:43:10 +0200

    Fix sully for k < 0

--- ASM/Sully.s
+++ ASM/Sully.s
@@ -2,7 +2,7 @@ section .data
 c db "nasm64 -f macho64 -dREP Sully_%1$d.s ; gcc Sully_%1$d.o -o Sully_%1$d ; rm -rf Sully_%1$d.o",0
 e db "./Sully_%d",0
 p db "Sully_%d.s",0
-s db "section .data%6$cc db %5$c%1$s%5$c,0%6$ce db %5$c%2$s%5$c,0%6$cp db %5$c%3$s%5$c,0%6$cs db %5$c%4$s%5$c,0%6$cw db %5$cw%5$c,0%6$csection .bss%6$cb:resb 128%6$csection .text%6$cextern _exit%6$cextern _fclose%6$cextern _fopen%6$cextern _fprintf%6$cextern _sprintf%6$cextern _system%6$cglobal _main%6$c_main:%6$cpush rbp%6$cmov rbp,rsp%6$cmov r15,%7$c%6$c%%ifdef REP%6$cdec r15%6$c%%endif%6$clea rdi,[rel b]%6$clea rsi,[rel p]%6$cmov rdx,r15%6$ccall _sprintf%6$clea rdi,[rel b]%6$clea rsi,[rel w]%6$ccall _fopen%6$ccmp rax,0%6$cjz .exit%6$cmov rbx,rax%6$cmov rdi,rbx%6$clea rsi,[rel s]%6$clea rdx,[rel c]%6$clea rcx,[rel e]%6$clea r8,[rel p]%6$clea r9,[rel s]%6$cpush 0%6$cadd r15,48%6$cpush r15%6$csub r15,48%6$cpush 10%6$cpush 34%6$ccall _fprintf%6$cmov rdi,rbx%6$ccall _fclose%6$clea rdi,[rel b]%6$clea rsi,[rel c]%6$cmov rdx,r15%6$ccall _sprintf%6$clea rdi,[rel b]%6$ccall _system%6$ccmp r15,0%6$cje .exit%6$clea rdi,[rel b]%6$clea rsi,[rel e]%6$cmov rdx,r15%6$ccall _sprintf%6$clea rdi,[rel b]%6$ccall _system%6$c.exit:%6$ccall _exit%6$c",0
+s db "section .data%6$cc db %5$c%1$s%5$c,0%6$ce db %5$c%2$s%5$c,0%6$cp db %5$c%3$s%5$c,0%6$cs db %5$c%4$s%5$c,0%6$cw db %5$cw%5$c,0%6$csection .bss%6$cb:resb 128%6$csection .text%6$cextern _exit%6$cextern _fclose%6$cextern _fopen%6$cextern _fprintf%6$cextern _sprintf%6$cextern _system%6$cglobal _main%6$c_main:%6$cpush rbp%6$cmov rbp,rsp%6$cmov r15,%7$c%6$ccmp r15,0%6$cjl .exit%6$c%%ifdef REP%6$cdec r15%6$c%%endif%6$clea rdi,[rel b]%6$clea rsi,[rel p]%6$cmov rdx,r15%6$ccall _sprintf%6$clea rdi,[rel b]%6$clea rsi,[rel w]%6$ccall _fopen%6$ccmp rax,0%6$cjz .exit%6$cmov rbx,rax%6$cmov rdi,rbx%6$clea rsi,[rel s]%6$clea rdx,[rel c]%6$clea rcx,[rel e]%6$clea r8,[rel p]%6$clea r9,[rel s]%6$cpush 0%6$cadd r15,48%6$cpush r15%6$csub r15,48%6$cpush 10%6$cpush 34%6$ccall _fprintf%6$cmov rdi,rbx%6$ccall _fclose%6$clea rdi,[rel b]%6$clea rsi,[rel c]%6$cmov rdx,r15%6$ccall _sprintf%6$clea rdi,[rel b]%6$ccall _system%6$ccmp r15,0%6$cje .exit%6$clea rdi,[rel b]%6$clea rsi,[rel e]%6$cmov rdx,r15%6$ccall _sprintf%6$clea rdi,[rel b]%6$ccall _system%6$c.exit:%6$ccall _exit%6$c",0
 w db "w",0
 section .bss
 b:resb 128
@@ -18,6 +18,8 @@ _main:
 push rbp
 mov rbp,rsp
 mov r15,5
+cmp r15,0
+jl .exit
 %ifdef REP
 dec r15
 %endif
--- C/Sully.c
+++ C/Sully.c
@@ -1,8 +1,9 @@
 #include<fcntl.h>
 #include<stdio.h>
 #include<stdlib.h>
-int k=5;
+int k=-1;
 int main(void){
+if(k<0)return -1;
 #ifdef REP
 k-=1;
 #endif
@@ -10,7 +11,7 @@ char buff[99];
 sprintf(buff,"Sully_%d.c",k);
 FILE *f=fopen(buff,"w");
 if(!f)return -1;
-const char *s="#include<fcntl.h>%1$c#include<stdio.h>%1$c#include<stdlib.h>%1$cint k=%4$d;%1$cint main(void){%1$c#ifdef REP%1$ck-=1;%1$c#endif%1$cchar buff[99];%1$csprintf(buff,%2$cSully_%%d.c%2$c,k);%1$cFILE *f=fopen(buff,%2$cw%2$c);%1$cif(!f)return -1;%1$cconst char *s=%2$c%3$s%2$c;%1$cfprintf(f,s,10,34,s,k);%1$cfclose(f);%1$csprintf(buff,%2$cgcc -Wall -Wextra -Werror -DREP Sully_%%d.c -o Sully_%%d%2$c,k,k);%1$csystem(buff);%1$cif(k!=0){sprintf(buff,%2$c./Sully_%%d%2$c,k);system(buff);}%1$c}%1$c";
+const char *s="#include<fcntl.h>%1$c#include<stdio.h>%1$c#include<stdlib.h>%1$cint k=%4$d;%1$cint main(void){%1$cif(k<0)return -1;%1$c#ifdef REP%1$ck-=1;%1$c#endif%1$cchar buff[99];%1$csprintf(buff,%2$cSully_%%d.c%2$c,k);%1$cFILE *f=fopen(buff,%2$cw%2$c);%1$cif(!f)return -1;%1$cconst char *s=%2$c%3$s%2$c;%1$cfprintf(f,s,10,34,s,k);%1$cfclose(f);%1$csprintf(buff,%2$cgcc -Wall -Wextra -Werror -DREP Sully_%%d.c -o Sully_%%d%2$c,k,k);%1$csystem(buff);%1$cif(k!=0){sprintf(buff,%2$c./Sully_%%d%2$c,k);system(buff);}%1$c}%1$c";
 fprintf(f,s,10,34,s,k);
 fclose(f);
 sprintf(buff,"gcc -Wall -Wextra -Werror -DREP Sully_%d.c -o Sully_%d",k,k);
